a8cb67862a818b87907570b4cf7ae24cb702ffd6
Elie NEHME <elie29@gmail.com>
2018-11-02 10:10:58 +0100

    Added Math.max

--- src/vacation.ts
+++ src/vacation.ts
@@ -6,39 +6,33 @@ export class Vacation {
     this.position = position;
     this.coefficient = coefficient;
 
-    let numberOfDaysOff = 25;
-
-    if (this.level5) {
-      numberOfDaysOff = 30;
-    } else if (this.level4) {
-      numberOfDaysOff = 29;
-    } else if (this.level3) {
-      numberOfDaysOff = 28;
-    } else if (this.level2) {
-      numberOfDaysOff = 27;
-    } else if (this.level1) {
-      numberOfDaysOff = 26;
-    }
-    return numberOfDaysOff;
+    return Math.max(
+      25,
+      this.level1,
+      this.level2,
+      this.level3,
+      this.level4,
+      this.level5
+    );
   }
 
-  protected get level1(): boolean {
-    return this.position > 1 || this.coefficient > 130;
+  protected get level1(): number {
+    return this.position > 1 || this.coefficient > 130 ? 26 : 0;
   }
 
-  protected get level2(): boolean {
-    return this.position > 2 || this.coefficient > 180;
+  protected get level2(): number {
+    return this.position > 2 || this.coefficient > 180 ? 27 : 0;
   }
 
-  protected get level3(): boolean {
-    return this.position > 3 || this.coefficient > 200;
+  protected get level3(): number {
+    return this.position > 3 || this.coefficient > 200 ? 28 : 0;
   }
 
-  protected get level4(): boolean {
-    return this.position > 4 || this.coefficient > 310;
+  protected get level4(): number {
+    return this.position > 4 || this.coefficient > 310 ? 29 : 0;
   }
 
-  protected get level5(): boolean {
-    return this.position > 5 || this.coefficient > 420;
+  protected get level5(): number {
+    return this.position > 5 || this.coefficient > 420 ? 30 : 0;
   }
 }
